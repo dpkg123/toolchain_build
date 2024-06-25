a4bdc0a98e6c07b11036c5eb2c96c8cc2965b177
טפל בלוט <130299352+dpkg123@users.noreply.github.com>
2024-06-25 09:15:28 +0800

    Update build_electron_toolchain.sh

--- scripts/build_electron_toolchain.sh
+++ scripts/build_electron_toolchain.sh
@@ -23,8 +23,3 @@ echo "正在构建32位binutils中"
 mkdir build-binutils && cd build-binutils
 env CFLAGS=-O2 CXXFLAGS=-O2 ../$BV/configure --prefix=$IN --disable-compressed-debug-sections --disable-gdb --disable-werror --enable-deterministic-archives --enable-new-dtags --enable-plugins --enable-threads --quiet --with-system-zlib --disable-multilib --disable-nls --with-gnu-as --with-gnu-ld --program-prefix=arm-linux-gnueabi- --target=arm-linux-gnueabi --with-pkgversion="Electron Binutils"
 make -j8 && make install
-
-echo "正在打包压缩binutils中"
-cd /root/Toolchain
-tar -I pixz -cf electron-$BV.tar.xz electron-$BV
-rm -rf electron-$BV
